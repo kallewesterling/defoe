91e0d4c49338aa4eb13ed45b0bb7646db00707aa
Kalle Westerling <7298727+kallewesterling@users.noreply.github.com>
2022-02-23 17:22:31 +0000

    Troubleshooting with the help of type hinting

--- defoe/fmp/queries/weighted_target_segmentation.py
+++ defoe/fmp/queries/weighted_target_segmentation.py
@@ -8,6 +8,7 @@ from pyspark.rdd import PipelinedRDD
 
 from defoe import query_utils
 from defoe.fmp import Document
+from defoe.fmp.textblock import TextBlock
 
 from defoe.fmp.query_utils import (
     segment_image,
@@ -22,132 +23,25 @@ from itertools import product
 import os
 
 
-get_highlight_coords = lambda target_loc, keyword_loc: [
-    (target_loc.x, target_loc.y, target_loc.w, target_loc.h),
-    (keyword_loc.x, keyword_loc.y, keyword_loc.w, keyword_loc.h),
-]
-get_highlight_coords.__doc__ = (
-    """ Parses out the coordinates for highlights around target words and keywords """
-)
-
-
-'''
-# compute_distance provides the absolute distance between `position` attributes of two WordLocation objects
-compute_distance = lambda k_loc, t_loc: abs(k_loc.position - t_loc.position)
-
-
-def get_min_distance_to_target(keyword_locations: list, target_locations: list):
+def get_highlight_coords(target_loc: WordLocation, keyword_loc: WordLocation) -> list:
     """
-    # TODO #3: add missing docstring
+    Parses out the coordinates for highlights around target words and keywords.
     """
 
-    # TODO: I have a feeling this could be refactored with `functools.reduce`
-    min_distance, target_loc, keyword_loc = None, None, None
-    for k_loc in keyword_locations:
-        for t_loc in target_locations:
-            word_distance = compute_distance(k_loc, t_loc)
-
-            if not min_distance or word_distance < min_distance:
-                min_distance = word_distance
-                target_loc = t_loc
-                keyword_loc = k_loc
-
-    return min_distance, target_loc, keyword_loc
+    return [
+        (target_loc.x, target_loc.y, target_loc.w, target_loc.h),
+        (keyword_loc.x, keyword_loc.y, keyword_loc.w, keyword_loc.h),
+    ]
 
 
-def find_words_in_document(
-    document: Document,
-    target_words: list,
-    keywords: list,
-    preprocess_type: PreprocessWordType = PreprocessWordType.LEMMATIZE,
-):
+def check_word(word, lst, fuzzy=False) -> bool:
     """
-    If a keyword occurs more than once on a page, there will be only
-    one tuple for the page for that keyword.
+    Fuzzy/non-fuzzy check of word against a list.
 
-    If more than one keyword occurs on a page, there will be one tuple
-    per keyword.
-
-    The distance between keyword and target word is recorded in the output tuple.
-
-    :param document: document
-    :type document: defoe.fmp.document.Document
-    :param target_words: # TODO #3
-    :type target_words: # TODO #3
-    :param keywords: keywords
-    :type keywords: list(str)
-    :param preprocess_type: how words should be preprocessed
-    (normalize, normalize and stem, normalize and lemmatize, none)
-    :type preprocess_type: defoe.query_utils.PreprocessWordType
-    :return: list of tuples
-    :rtype: list(tuple)
+    :return: boolean determining whether a word exists in a list
+    :rtype: bool
     """
 
-    matches = []
-    for article_id, article in document.articles.items():
-        for tb in article:
-            preprocessed_data = [
-                (
-                    x[0],
-                    x[1],
-                    x[2],
-                    x[3],
-                    preprocess_word(x[4], preprocess_type),
-                    position,
-                )
-                for position, x in enumerate(tb.locations)
-            ]
-
-            keywords_matched = defaultdict(lambda: [])
-            targetwords_matched = []
-            for x, y, w, h, word, position in preprocessed_data:
-                loc = WordLocation(
-                    word=word,
-                    position=position,
-                    year=document.year,
-                    document=document,
-                    article=article_id,
-                    textblock_id=tb.textblock_id,
-                    textblock_coords=tb.textblock_coords,
-                    textblock_page_area=tb.textblock_page_area,
-                    textblock_page_name=tb.page_name,
-                    x=x,
-                    y=y,
-                    w=w,
-                    h=h,
-                )
-
-                # TODO: Only absolute match. We may want to consider fuzzy matching here as well
-                if word != "" and word in keywords:
-                    keywords_matched[word].append(loc)
-
-                if word != "" and word in target_words:
-                    targetwords_matched.append(loc)
-
-            for locations in keywords_matched.values():
-                min_distance, target_loc, keyword_loc = get_min_distance_to_target(
-                    locations, targetwords_matched
-                )
-
-                if min_distance:
-                    matches.append(
-                        MatchedWords(
-                            target_word=target_loc.word,
-                            keyword=keyword_loc.word,
-                            textblock=target_loc,
-                            distance=min_distance,
-                            words=tb.words,
-                            preprocessed=preprocessed_data,
-                            highlight=get_highlight_coords(target_loc, keyword_loc),
-                        )
-                    )
-
-    return matches
-'''
-
-
-def check_word(word, lst, fuzzy=False):
-    """ Fuzzy/non-fuzzy check of word against a list """
     if not word:
         return False
 
@@ -160,22 +54,31 @@ def check_word(word, lst, fuzzy=False):
             return True
 
 
-get_word_data = lambda loc, document, article_id, tb: WordLocation(
-    word=loc[4],
-    position=loc[5],
-    document=document,
-    year=document.year,
-    article=article_id,
-    textblock_id=tb.textblock_id,
-    textblock_coords=tb.textblock_coords,
-    textblock_page_area=tb.textblock_page_area,
-    textblock_page_name=tb.page_name,
-    x=loc[0],
-    y=loc[1],
-    w=loc[2],
-    h=loc[3],
-)
-get_word_data.__doc__ = """ Extracts a WordLocation object from a tb.location object, combined with data from document, article, and tb. """
+def get_word_data(
+    loc: TextBlock.locations, document: Document, article_id: str, textblock: TextBlock
+) -> WordLocation:
+    """
+    Combines data from a tb.location object, a Document, its article, and TextBlock.
+
+    :return: a WordLocation object that describes the word's data in-depth.
+    :rtype: defoe.fmp.query_utils.WordLocation
+    """
+
+    return WordLocation(
+        word=loc[4],
+        position=loc[5],
+        document=document,
+        year=document.year,
+        article=article_id,
+        textblock_id=textblock.textblock_id,
+        textblock_coords=textblock.textblock_coords,
+        textblock_page_area=textblock.textblock_page_area,
+        textblock_page_name=textblock.page_name,
+        x=loc[0],
+        y=loc[1],
+        w=loc[2],
+        h=loc[3],
+    )
 
 
 def find_closest(
@@ -185,7 +88,7 @@ def find_closest(
     preprocess_type: PreprocessWordType = PreprocessWordType.LEMMATIZE,
     fuzzy_target: bool = False,
     fuzzy_keyword: bool = False,
-):
+) -> list:
     """
     Updated query structure (replaces the earlier one, above). This structure allows for
     fuzzy searching, and is faster (?).
